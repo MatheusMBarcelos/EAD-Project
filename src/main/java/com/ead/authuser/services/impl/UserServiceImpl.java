048af34e4805eb842dfd912ae9cf7f663b36c3fa
Matheus <matheus.sl.dh@gmail.com>
2022-05-22 11:59:01 -0300

    Deleção em cascata de usuário e sua relação com curso.

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,26 +1,31 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final UserCourseRepository userCourseRepository;
 
     @Override
-    public Page<UserModel> findAll( Specification<UserModel> spec, Pageable pageable) {
+    public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
         return userRepository.findAll(spec, pageable);
     }
 
@@ -29,9 +34,14 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(userId);
     }
 
+    @Transactional
     @Override
-    public void delete(UserModel userId) {
-        userRepository.delete(userId);
+    public void delete(UserModel userModel) {
+        List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
+        if (!userCourseModelList.isEmpty()) {
+            userCourseRepository.deleteAll(userCourseModelList);
+        }
+        userRepository.delete(userModel);
     }
 
     @Override
